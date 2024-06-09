c0698c92bab467eb683085f6ddc0ca4f20634099
James Costa <jamescosta4747@gmail.com>
2024-06-09 20:32:23 -0300

    Adicionado setor do Usuario Admin

--- src/BibliotecaBase.java
+++ src/BibliotecaBase.java
@@ -12,10 +12,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public  class BibliotecaBase extends JFrame {
+public class BibliotecaBase extends JFrame {
     protected JPanel BibliotecaPanel;
     protected JLabel Titulo;
     protected JLabel opcoes;
+    private JToolBar navToolBar;
     protected JLabel TituloBiblio;
     protected JLabel image;
     protected JTable tabelaLivros;
@@ -27,12 +28,32 @@ public  class BibliotecaBase extends JFrame {
     public BibliotecaBase(String nomeUsuario, boolean isAdmin) {
         // Inicializar o painel principal
         BibliotecaPanel = new JPanel();
-        BibliotecaPanel.setLayout(new BoxLayout(BibliotecaPanel, BoxLayout.Y_AXIS));
+        BibliotecaPanel.setLayout(new BorderLayout());
         setContentPane(BibliotecaPanel);
-        setSize(600, 400); // Ajuste o tamanho conforme necessário
+        setSize(600, 400);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
+
+        // Inicializar os componentes
+        Titulo = new JLabel("Saraijavas");
+        opcoes = new JLabel("Menu");
+        TituloBiblio = new JLabel();
+        tabelaLivrosModel = new DefaultTableModel(new Object[]{"ID", "Título", "Autor", "Preço", "Categoria"}, 0) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+        tabelaLivros = new JTable(tabelaLivrosModel);
+        JScrollPane scrollPane = new JScrollPane(tabelaLivros);
+
+        // Adicionar componentes ao painel
+        tituloOpcoesPanel = new JPanel(new BorderLayout());
+        tituloOpcoesPanel.add(Titulo, BorderLayout.WEST);
+        tituloOpcoesPanel.add(opcoes, BorderLayout.EAST);
+        BibliotecaPanel.add(tituloOpcoesPanel, BorderLayout.NORTH);
+        BibliotecaPanel.add(scrollPane, BorderLayout.CENTER);
 
         // Criar instância de UsuarioCliente
         usuarioCliente = new UsuarioCliente(nomeUsuario, "senha"); // Ajuste conforme necessário para obter a senha
@@ -40,27 +61,13 @@ public  class BibliotecaBase extends JFrame {
         // Criar e configurar o menu pop-up
         JPopupMenu popupMenu = new JPopupMenu();
         JMenuItem menuItem1 = new JMenuItem("Home");
-        JMenuItem menuItem2 = new JMenuItem("Biblioteca Pessoal");
-        JMenuItem menuItem3 = new JMenuItem("Saldo: R$ " + usuarioCliente.getSaldo()); // Exibe o saldo atual
         JMenuItem menuItem4 = new JMenuItem("Sair");
 
         popupMenu.add(menuItem1);
-        popupMenu.add(menuItem2);
-        popupMenu.add(menuItem3);
         popupMenu.add(menuItem4);
 
         menuItem1.addActionListener(e -> {
-            new ClienteHome(nomeUsuario,isAdmin);
-            dispose();
-        });
-
-        menuItem2.addActionListener(e -> {
-            new BibliotecaBase(nomeUsuario, isAdmin);
-            dispose();
-        });
-
-        menuItem3.addActionListener(e -> {
-            new ClienteDeposito(nomeUsuario);
+            new AdminHome(nomeUsuario);
             dispose();
         });
 
@@ -68,6 +75,26 @@ public  class BibliotecaBase extends JFrame {
             new MainInterface();
             dispose();
         });
+
+// Adicionar as opções específicas do cliente (não administrador)
+        if (!isAdmin) {
+            JMenuItem menuItem2 = new JMenuItem("Biblioteca Pessoal");
+            JMenuItem menuItem3 = new JMenuItem("Saldo: R$ " + usuarioCliente.getSaldo());
+
+            popupMenu.add(menuItem2);
+            popupMenu.add(menuItem3);
+
+            menuItem2.addActionListener(e -> {
+                new BibliotecaBase(nomeUsuario, isAdmin);
+                dispose();
+            });
+
+            menuItem3.addActionListener(e -> {
+                new ClienteDeposito(nomeUsuario);
+                dispose();
+            });
+        }
+
 
         // Adicionar um ouvinte de mouse à JLabel "opcoes" para exibir o menu pop-up quando o botão direito do mouse for clicado
         opcoes.addMouseListener(new MouseAdapter() {
@@ -78,15 +105,8 @@ public  class BibliotecaBase extends JFrame {
             }
         });
 
-        // Criar um JPanel para Titulo e opcoes
-        tituloOpcoesPanel = new JPanel(new BorderLayout());
-        tituloOpcoesPanel.add(Titulo, BorderLayout.WEST);
-        tituloOpcoesPanel.add(opcoes, BorderLayout.EAST);
-
         if (isAdmin) {
             initComponents(); // Inicializar componentes do admin
-
-            // Carregar todos os livros do banco de dados
             carregarTodosLivros();
         } else {
             initComponentsCliente(); // Inicializar componentes do cliente
@@ -94,30 +114,21 @@ public  class BibliotecaBase extends JFrame {
     }
 
     protected void initComponents() {
-        // Inicializar componentes do admin
         btnAlterarStatus = new JButton("Alterar Status");
         btnAlterarStatus.addActionListener(e -> {
-            // Aqui você pode adicionar a lógica para alterar o status dos livros selecionados
-            // Por exemplo:
             int[] selectedRows = tabelaLivros.getSelectedRows();
             for (int row : selectedRows) {
-                // Obtenha o ID do livro na linha selecionada
                 int livroId = (int) tabelaLivros.getValueAt(row, 0); // Supondo que o ID esteja na primeira coluna
-                // Defina o status is_deleted como verdadeiro para o livro com o ID correspondente
                 definirStatusLivro(livroId, true);
             }
         });
 
-        // Adicionar o botão ao painel principal
         BibliotecaPanel.add(btnAlterarStatus, BorderLayout.SOUTH);
     }
 
     protected void initComponentsCliente() {
-        // Inicializar componentes do cliente
         TituloBiblio = new JLabel("Livros Alugados");
-        image = new JLabel(new ImageIcon("caminho/para/sua/imagem.jpg")); // Ajuste o caminho da imagem
 
-        // Inicializar e configurar a tabela e seu modelo
         tabelaLivrosModel = new DefaultTableModel(new Object[]{"Título", "Autor", "Preço", "Categoria"}, 0) {
             @Override
             public boolean isCellEditable(int row, int column) {
@@ -127,13 +138,10 @@ public  class BibliotecaBase extends JFrame {
         tabelaLivros = new JTable(tabelaLivrosModel);
         JScrollPane scrollPane = new JScrollPane(tabelaLivros);
 
-        // Adicionar componentes ao painel principal
-        BibliotecaPanel.add(tituloOpcoesPanel);
-        BibliotecaPanel.add(TituloBiblio);
-        BibliotecaPanel.add(image);
-        BibliotecaPanel.add(scrollPane);
+        BibliotecaPanel.add(TituloBiblio, BorderLayout.NORTH);
+        BibliotecaPanel.add(image, BorderLayout.CENTER);
+        BibliotecaPanel.add(scrollPane, BorderLayout.SOUTH);
 
-        // Carregar livros alugados pelo usuário ao criar a biblioteca pessoal
         carregarLivrosAlugados();
     }
 
@@ -143,7 +151,6 @@ public  class BibliotecaBase extends JFrame {
             conexao = Conexao.conectar();
             ResultSet rs = listarTodosLivros(conexao);
             while (rs.next()) {
-                // Criar um objeto Livro com os dados do resultado da consulta
                 Livro livro = new Livro();
                 livro.setId(rs.getInt("id"));
                 livro.setTitulo(rs.getString("titulo"));
@@ -151,7 +158,6 @@ public  class BibliotecaBase extends JFrame {
                 livro.setCategoria(rs.getString("categoria"));
                 livro.setValor(rs.getDouble("preco"));
 
-                // Adicionar o livro à tabela
                 tabelaLivrosModel.addRow(new Object[]{livro.getId(), livro.getTitulo(), livro.getAutor(), livro.getValor(), livro.getCategoria()});
             }
         } catch (SQLException ex) {
@@ -160,15 +166,12 @@ public  class BibliotecaBase extends JFrame {
             Conexao.fecharConexao(conexao);
         }
     }
+
     protected ResultSet listarTodosLivros(Connection conexao) throws SQLException {
-        // Defina a consulta SQL para listar todos os livros
         String sql = "SELECT * FROM livros";
-        // Crie uma declaração preparada
         PreparedStatement statement = conexao.prepareStatement(sql);
-        // Execute a consulta e retorne o conjunto de resultados
         return statement.executeQuery();
     }
-
 
     protected void carregarLivrosAlugados() {
         Connection conexao = null;
@@ -183,9 +186,7 @@ public  class BibliotecaBase extends JFrame {
                 double preco = rs.getDouble("preco");
                 livro.setValor(preco);
 
-                // Adicionar o livro à tabela
-                tabelaLivrosModel.addRow(new Object[]{livro.getTitulo(), livro.getAutor(),
-                        livro.getValor(), livro.getCategoria()});
+                tabelaLivrosModel.addRow(new Object[]{livro.getTitulo(), livro.getAutor(), livro.getValor(), livro.getCategoria()});
             }
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(this, "Erro ao carregar livros: " + ex.getMessage());
@@ -203,6 +204,20 @@ public  class BibliotecaBase extends JFrame {
             statement.setBoolean(1, isDeleted);
             statement.setInt(2, livroId);
             int linhasAfetadas = statement.executeUpdate();
+
+            // Se a atualização foi bem-sucedida e o livro foi marcado como excluído,
+            // remova a linha correspondente da tabela
+            if (linhasAfetadas > 0 && isDeleted) {
+                // Encontra a linha na tabela que corresponde ao livroId
+                for (int i = 0; i < tabelaLivros.getRowCount(); i++) {
+                    if ((int) tabelaLivros.getValueAt(i, 0) == livroId) { // Assumindo que o ID do livro está na primeira coluna
+                        // Remove a linha da tabela
+                        tabelaLivrosModel.removeRow(i);
+                        break; // Uma vez que a linha foi removida, não há necessidade de continuar procurando
+                    }
+                }
+            }
+
             return linhasAfetadas > 0;
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(this, "Erro ao alterar status do livro: " + ex.getMessage());
@@ -210,5 +225,10 @@ public  class BibliotecaBase extends JFrame {
         } finally {
             Conexao.fecharConexao(conexao);
         }
+    }
+
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new BibliotecaBase("Admin", true));
     }
 }
